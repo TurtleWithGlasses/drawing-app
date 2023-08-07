bcde71523cd3a0ac2db6fa7eb0b381c641e35980
Mehmet Ali Soylu <mhmtsoylu1928@gmail.com>
2023-08-07 15:03:14 +0300

    Create change color, thickness, undo and clear functions

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import tkinter.ttk as ttk
 
 class free_hand_drawing(tk.Tk):
-    def __int__(self):
+    def __init__(self):
         super().__init__()
         self.title("Free Hand Drawing Tool")
         self._xold = None
@@ -23,7 +23,7 @@ class free_hand_drawing(tk.Tk):
                                   state="readonly", width=10)
         color_list.current(0)
         self.option_add("*TCombobox*Listbox.selectBackground","skyblue")
-        color_list.bind("<<CombovoxSelected>>", self._change_color)
+        color_list.bind("<<ComboboxSelected>>", self._change_color)
         color_list.grid(row=0, column=0, padx=5)
 
         self.t_select = tk.StringVar()
@@ -41,13 +41,26 @@ class free_hand_drawing(tk.Tk):
                   activebackground="blue4", activeforeground="white",
                   command=self._clear).grid(row=0, column=3, padx=5)
 
-        self.canvas = tk.canvas(self, width=500, height=500, bg="white")
+        self.canvas = tk.Canvas(self, width=500, height=500, bg="white")
         self.canvas.grid(row=1, column=0, padx=10, pady=(0, 10))
         self.canvas.bind("<>", self._on_release)
         self.canvas.bind("<B1-Motion>", self._on_movement)
 
+    def _change_color(self, event=None):
+        self.color = self.col_select.get()
 
+    def _change_thickness(self, event=None):
+        self.thickness = int(self.t_select.get())
 
+    def _clear(self):
+        self.canvas.delete("all")
+        self.tag = ["tag", "0"]
+
+    def _undo(self):
+        current_tag = int(self.tag[1])
+        if current_tag >= 1:
+            self.canvas.delete("tag" + str(current_tag-1))
+            self.tag = ["tag", str(current_tag - 1)]
 
 
 free_hand_drawing().mainloop()
